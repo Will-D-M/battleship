a4a9b0e70f4aecfc82e75f78025cb97f3a5919be
William Meighan <williamdmeighan@gmail.com>
2019-12-02 18:50:58 -0700

    Add test_it_has_a_length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -13,7 +13,11 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_has_a_name
-    assert_equal "Cruiser", @cruiser
+    assert_equal "Cruiser", @cruiser.name
+  end
+
+  def test_it_has_a_length
+    assert_equal 3, @cruiser.length
   end
 
 end
