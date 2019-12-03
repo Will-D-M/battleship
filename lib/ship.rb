b20bb41dcc70eca96f5893a68fe648316b9be252
Anthony Gomes <code@tonygom.es>
2019-12-03 14:43:22 -0700

    Add refactored Ship Test & Ship Class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,22 +1,26 @@
 class Ship
-  attr_accessor :name, :length, :health, :sunk
+  attr_reader :name, :length
+  attr_accessor :health, :sunk
 
   def initialize(name, length)
     @name = name
     @length = length
-    @health = 3
+    @health = length
     @sunk = false
   end
 
   def sunk?
-    @sunk
+    if @health == 0
+      @sunk = true
+    else
+      @sunk
+    end
   end
 
   def hit
     if @health > 0 && @health <= 3
       @health -= 1
     end
-      @sunk = true
   end
 
 end
