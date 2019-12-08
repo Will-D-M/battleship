c11da0697688cbec6a357ae304b78d2238f7c272
Anthony Gomes <code@tonygom.es>
2019-12-08 16:41:49 -0700

    Add updates to User class & tests

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,6 @@
+require_relative 'ship'
+require_relative 'board'
+
 class User
   attr_reader :computer, :player
 
@@ -8,19 +11,37 @@ class User
     @player_turns = {}
   end
 
-  # def computer_player_start
-  #   @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-  #   @comp_cruiser = Ship.new("Cruiser", 3)
-  #   @comp_submarine = Ship.new("Submarine", 2)
-  #
-  #   @comp_cruiser_move = @board_coords.sample(3)
-  #   @comp_sub_move = @board_coords.sample(2)
-  #   @board.place(@comp_cruiser, @comp_cruiser_move)
-  #   @board.place(@comp_submarine, @comp_sub_move)
-  # end
-  #
-  # def computer_player_moves
-  #   @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-  #   @comp_move = @board_coords.sample
-  # end
+  def computer_player_start
+    @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    @comp_cruiser = Ship.new("Cruiser", 3)
+    @comp_sub = Ship.new("Submarine", 2)
+
+    @comp_cruiser_placer = @board_coords.sample(3)
+    @comp_sub_placer = @board_coords.sample(2)
+    @board.place(@comp_cruiser, @comp_cruiser_placer)
+    @board.place(@comp_sub, @comp_sub_placer)
+  end
+
+  def computer_player_moves_and_turns
+    @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    @comp_move = @board_coords.sample
+    @board_coords.delete(@comp_move)
+    @comp_turns[@comp_move] = @board.cells.valid_placement?
+  end
+
+  def human_player_start(player_cruiser_placer, player_sub_placer)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_sub = Ship.new("Submarine", 2)
+
+    @board.place(@player_cruiser, player_cruiser_placer)
+    @board.place(@player_sub, player_sub_placer)
+  end
+
+  def human_player_turns(player_move)
+    @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    # Add logic for repeat turns
+    @player_turns[@player_moves] = @board.cells
+    @board_coords.delete(@player_move)
+    @player_turns[@player_move] = @board.cells.valid_placement?
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/user'
 require './lib/board'
-require './lib/game'
+# require './lib/game'
 class UserTest < MiniTest::Test
 
     def setup
@@ -13,5 +13,16 @@ class UserTest < MiniTest::Test
     def test_user_exists
       assert_instance_of User, @player
       assert_instance_of User, @computer
+    end
+
+    def test_computer_player_start_method
+      assert_equal Ship, @comp_cruiser
+      assert_equal Ship, @comp_sub
+    end
+
+    def test_computer_player_sample_method_random_moves
+      @board_coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+
+      assert @board_coords.include?(@comp_sample_move)
     end
 end
