174bf28f11b602e625c6d020a8d0781fded37718
William Meighan <williamdmeighan@gmail.com>
2019-12-08 16:22:22 -0700

    Add game funcitonality... using this class for now just to make sure it is running properly but will eventiually create a runner

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,6 @@
-# game runner file
 require_relative 'board'
-# require_relative 'user'
-
+require_relative 'ship'
+require_relative 'player'
 
   def main_menu
     3.times{puts "\n"}
@@ -10,7 +9,7 @@ require_relative 'board'
     print "> "
     menu_selection = gets.chomp.to_s.downcase
 
-    until menu_selection == "p" || menu_selection == "q" do
+    until menu_selection == "p" || menu_selection == "q"
       puts "Please enter a valid selection"
       print "> "
       menu_selection = gets.chomp.to_s.downcase
@@ -19,45 +18,132 @@ require_relative 'board'
     if menu_selection == "q"
       puts "\n\nGoodbye.\n\n\n"
       return
-    elsif menu_selection == "p"
-      puts "\n\n...game loading...\n\n\n"
-      sleep(3)
-      # @board.render
-      # computer = User.new("Computer")
-      # player = User.new("Player")
-      # computer.board.place
-      puts "Computer is placing ships on their grid...\n"
-      sleep(3)
-      puts "The computer's ships are on the grid.\n\n\n"
-      sleep(2)
-      puts "Now it's your turn. You have two ships.\n\n\n"
-      sleep(3)
-      puts "Your Cruiser is three units long.\n"
-      puts "For example:\n\nA1 A2 A3\nor\nA1 B1 C1\n\n"
-      puts "Press Enter/Return To Continue"
-      continue = gets
-      print "> "
+    else
+      setup
+    end
+  end
 
-        if continue == "\n"
-          puts "\n\nGreat!\n\n"
+  def setup
+      puts "\n\n...game loading...\n\n\n"
+      # sleep(3)
+      puts "Computer is placing ships on their grid...\n\n"
+
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
+
+      @computer_board = Board.new
+      @computer_board.add_cells
+
+      @computer_player = Player.new
+
+      @computer_player.add_ships(@cruiser)
+      @computer_player.add_ships(@submarine)
+
+      @computer_player.ships.each do |ship|
+        random_coordinates = @computer_board.cells.keys.sample(ship.length)
+        until @computer_board.valid_placement?(ship, random_coordinates)
+          random_coordinates = @computer_board.cells.keys.sample(ship.length)
         end
 
-      puts "Your Submarine is two units long.\n\n"
-      sleep(2)
-      puts "For example:\n\nA2 A3\nor\nB1 C1\n\n\n"
-      puts "Ready?\n\n"
-      puts "Press Enter/Return To Continue"
-      continue = gets
-      print "> "
-
-      if continue == "\n"
-        puts "\n\nGreat!\n\n"
+        @computer_board.place(ship, random_coordinates)
       end
+      # sleep(3)
+      puts "The computer's ships are on the grid.\n\n\n"
+      # sleep(2)
+      puts "Now it's your turn."
 
-      puts "Enter the squares for the Cruiser (3 spaces):"
+      @user_board = Board.new
+      @user_board.add_cells
+
+      @user_player = Player.new
+
+      @user_player.add_ships(@cruiser)
+      @user_player.add_ships(@submarine)
+      #render
+      # sleep(3)
+      puts "Your Cruiser is three units long.\n"
+      puts "For example:\n\nA1 A2 A3\nor\nA1 B1 C1\n\n"
+
+
+      puts @user_board.render
+
+      @user_player.ships.each do |ship|
+      puts "\nEnter the coordinates for the #{ship.name} (#{ship.length} spaces):"
       print "> "
+      user_coordinates = gets.chomp.upcase.split
+        until @user_board.valid_placement?(ship, user_coordinates)
+          puts "\nThose are invalid coordinates. Please try again:"
+          print "> "
+          user_coordinates = gets.chomp.upcase.split
+        end
+      @user_board.place(ship, user_coordinates)
+      puts "\n"
 
     end
+    turn
+  end
+
+  def turn
+    puts "\n==========CURRENT COMPUTER BOARD=========="
+    puts @computer_board.render
+    puts "\n===========CURRENT USER BOARD==========="
+    puts @user_board.render(true)
+    puts "\nEnter the coordinate for your shot:"
+    print "> "
+
+    coordinate = gets.chomp.upcase
+
+    if @computer_board.valid_coordinate?(coordinate) == true && !@computer_board.cells[coordinate].fired_upon?
+      coordinate
+
+    elsif !@computer_board.valid_coordinate?(coordinate)
+      until @computer_board.valid_coordinate?(coordinate) == true && !@computer_board.cells[coordinate].fired_upon?
+        puts "\nPlease enter a valid coordinate:"
+        print "> "
+        coordinate = gets.chomp.upcase
+      end
+      coordinate
+
+    elsif @computer_board.valid_coordinate?(coordinate) == true && @computer_board.cells[coordinate].fired_upon?
+      puts "\nYou have already fired upon this cell."
+      until @computer_board.valid_coordinate?(coordinate) == true && !@computer_board.cells[coordinate].fired_upon?
+        puts "\nPlease enter a valid coordinate:"
+        print "> "
+        coordinate = gets.chomp.upcase
+      end
+      coordinate
+    end
+
+    puts "\nFiring your missile..."
+    # sleep(1.5)
+
+    @computer_board.cells[coordinate].fire_upon
+    if @computer_board.cells[coordinate].render == "M"
+      result = "miss."
+    elsif @computer_board.cells[coordinate].render == "H"
+      result = "hit!"
+    elsif @computer_board.cells[coordinate].render == "X"
+      result = "hit and sunk my #{@computer_board.cells[coordinate].ship.name}!"
+    end
+
+    puts "\nYour shot on #{coordinate} was a #{result}"
+    # sleep(1.5)
+
+    puts "\nFiring my missile..."
+    # sleep(1.5)
+
+    @user_board.cells[coordinate].fire_upon
+    if @user_board.cells[coordinate].render == "M"
+      result = "miss."
+    elsif @user_board.cells[coordinate].render == "H"
+      result = "hit!"
+    elsif @user_board.cells[coordinate].render == "X"
+      result = "hit and sunk your #{@user_board.cells[coordinate].ship.name}!"
+    end
+
+    puts "\nMy shot on #{coordinate} was a #{result}"
+    # sleep(1.5)
+
   end
 
 main_menu
