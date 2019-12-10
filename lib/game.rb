a75fa64bee8dff2f84974a2608824ac7cdf97428
Anthony Gomes <code@tonygom.es>
2019-12-09 20:10:25 -0700

    Add restructured & clean formatted player file

--- lib/game.rb
+++ lib/game.rb
@@ -4,15 +4,17 @@ require_relative 'player'
 
 class Game
 
+  def initialize
+    @ships = []
+  end
+
 
   def welcome
     puts "Welcome to BATTLESHIP \n\n"
   end
 
-
-
   def main_menu
-    puts "Enter p to play. Enter q to quit. \n\n"
+    puts "Enter p to play. Enter q to quit.\n\n"
     print "> "
     menu_selection = gets.chomp.to_s.downcase
 
@@ -30,103 +32,84 @@ class Game
     end
   end
 
-
+  def add_ships(ship)
+    @ships << ship
+  end
 
   def setup
     puts "\n\n...game loading...\n\n"
-    # sleep(3)
+# sleep(3)
     puts "Computer is placing ships on their grid...\n\n"
-
-          # @computer_cruiser = Ship.new("Cruiser", 3)
-          # @computer_submarine = Ship.new("Submarine", 2)
-          #
-          # @computer_board = Board.new
-          # @computer_board.add_cells
-          #
-          # @computer_player = Player.new
-          #
-          # @computer_player.add_ships(@computer_cruiser)
-          # @computer_player.add_ships(@computer_submarine)
-          #
-          # @computer_player.ships.each do |ship|
-          #   random_coordinates = @computer_board.cells.keys.sample(ship.length)
-          #   until @computer_board.valid_placement?(ship, random_coordinates)
-          #     random_coordinates = @computer_board.cells.keys.sample(ship.length)
-          #   end
-          #   @computer_board.place(ship, random_coordinates)
-          # end
     # sleep(3)
+
+
+    @player.create_computer_player # Needs Correct Variable
+
     puts "The computer's ships are on the grid.\n\n"
-    # sleep(2)
+# sleep(3)
     puts "Now it's your turn.\n\n"
 
-        # @user_board = Board.new
-        # @user_board.add_cells
-        #
-        # @user_cruiser = Ship.new("Cruiser", 3)
-        # @user_submarine = Ship.new("Submarine", 2)
-        #
-        # @user_player = Player.new
-        #
-        # @user_player.add_ships(@user_cruiser)
-        # @user_player.add_ships(@user_submarine)
-    #render
-    # sleep(3)
+# sleep(3)
+    @player.create_user_player # Needs Correct Variable
+
     puts "Your Cruiser is three units long.\n"
     puts "For example:\n\nA1 A2 A3\nor\nA1 B1 C1\n\n"
 
-    puts @user_board.render
+    puts @user_board.render # This may need to point at player file
 
-    @user_player.ships.each do |ship|
-    puts "\nEnter the coordinates for the #{ship.name} (#{ship.length} spaces):"
-    print "> "
-    user_coordinates = gets.chomp.upcase.split
-      until @user_board.valid_placement?(ship, user_coordinates)
+    @user_player.ships.each do |ship| # This may need to point at player file
+      puts "\nEnter the coordinates for the #{ship.name} (#{ship.length} spaces):"
+      print "> "
+      user_coordinates = gets.chomp.upcase.split
+
+      until @user_board.valid_placement?(ship, user_coordinates) # This may need to point at player file
         puts "\nThose are invalid coordinates. Please try again:"
         print "> "
         user_coordinates = gets.chomp.upcase.split
       end
-      @user_board.place(ship, user_coordinates)
+
+      @user_board.place(ship, user_coordinates) # This may need to point at player file
     end
     user_turn
   end
 
-
-
   def user_turn
     puts "\n==========CURRENT COMPUTER BOARD=========="
-    puts @computer_board.render
+    puts @computer_board.render # This may need to point at player file
     puts "\n===========CURRENT USER BOARD==========="
-    puts @user_board.render(true)
+    puts @user_board.render(true) # This may need to point at player file
     puts "\nEnter the coordinate for your shot:"
     print "> "
 
     coordinate = gets.chomp.upcase
 
-          # if @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon?
-          #   coordinate
-          #
-          # elsif !@computer_board.valid_coordinate?(coordinate)
-          #   until @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon?
-          #     puts "\nPlease enter a valid coordinate:"
-          #     print "> "
-          #     coordinate = gets.chomp.upcase
-          #   end
-          #   coordinate
-          #
-    elsif @computer_board.valid_coordinate?(coordinate) && @computer_board.cells[coordinate].fired_upon?
-      puts "\nYou have already fired upon this cell."
-      until @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon?
+    if @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon? # This may need to point at player file
+      coordinate
+
+    elsif !@computer_board.valid_coordinate?(coordinate)
+      until @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon? # This may need to point at player file
         puts "\nPlease enter a valid coordinate:"
         print "> "
         coordinate = gets.chomp.upcase
       end
       coordinate
+
+    elsif @computer_board.valid_coordinate?(coordinate) && @computer_board.cells[coordinate].fired_upon? # This may need to point at player file
+      puts "\nYou have already fired upon this cell."
+
+      until @computer_board.valid_coordinate?(coordinate) && !@computer_board.cells[coordinate].fired_upon? # This may need to point at player file
+        puts "\nPlease enter a valid coordinate:"
+        print "> "
+        coordinate = gets.chomp.upcase
+      end
+    coordinate
     end
 
     puts "\nFiring your missile..."
-    # sleep(1.5)
-    @computer_board.cells[coordinate].fire_upon
+# sleep(3)
+
+    @computer_board.cells[coordinate].fire_upon # This may need to point at player file & where does .cells live after restructure?
+
     if @computer_board.cells[coordinate].render == "M"
       result = "miss."
     elsif @computer_board.cells[coordinate].render == "H"
@@ -136,47 +119,15 @@ class Game
     end
 
     puts "\nYour shot on #{coordinate} was a #{result}\n\n"
+# sleep(3)
 
-    if @computer_player.ships.all? { |ship| ship.sunk? }
+    if @computer_player.ships.all? { |ship| ship.sunk? } # This may need to point at player file
       puts "You win!\n\n"
       puts "Would you like to play again?\n\n"
       main_menu
     else
-      computer_turn
+      @player.computer_turn # Is this pointing correctly now? Or was it a different computer_turn value in this file?
     end
   end
-
-
-
-        # def computer_turn
-        #   puts "Firing my missile..."
-        #   # sleep(1.5)
-        #   comp_coordinate = @user_board.cells.keys.sample(1).join
-        #     until @user_board.valid_coordinate?(comp_coordinate) && !@user_board.cells[comp_coordinate].fired_upon?
-        #       comp_coordinate = @user_board.cells.keys.sample(1).join
-        #     end
-        #   comp_coordinate
-        #
-        #   @user_board.cells[comp_coordinate].fire_upon
-        #   if @user_board.cells[comp_coordinate].render == "M"
-        #     result = "miss."
-        #   elsif @user_board.cells[comp_coordinate].render == "H"
-        #     result = "hit!"
-        #   elsif @user_board.cells[comp_coordinate].render == "X"
-        #     result = "hit and sunk your #{@user_board.cells[comp_coordinate].ship.name}!"
-        #   end
-        #
-        #   puts "\nMy shot on #{comp_coordinate} was a #{result}\n\n"
-        #
-        #   if @user_player.ships.all? { |ship| ship.sunk? }
-        #     puts "I win!\n\n"
-        #     puts "Would you like to play again?\n\n"
-        #     main_menu
-        #   else
-        #     user_turn
-        #   end
-        # end
-
-
-
+@player.computer_turn # Comment this out to pry the file w/o erros & Ditto above
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,78 @@
 class Player
+  attr_reader :computer_player, :computer_board, :user_player, :user_board
 
-  attr_reader :ships
-
-  def initialize
-    @ships = []
+  def initialize # Not sure these should be nil values... they should just equal each other.
+    @computer_player = nil
+    @computer_board = nil
+    @user_player = nil
+    @user_board = nil
   end
 
-  def add_ships(ship)
-    @ships << ship
+  def create_user_player
+    @user_player = Player.new
+# May have to create Player instance in Game file? Not sure how to call. Ditto for Computer instance
+
+    @user_board = Board.new
+    @user_board.add_cells
+
+    @user_cruiser = Ship.new("Cruiser", 3)
+    @user_submarine = Ship.new("Submarine", 2)
+
+    @user_player.add_ships(@user_cruiser)
+    @user_player.add_ships(@user_submarine)
   end
 
+  def create_computer_player
+    @computer_player = Player.new
+
+    @computer_board = Board.new
+    @computer_board.add_cells
+
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+
+    @computer_player.add_ships(@computer_cruiser)
+    @computer_player.add_ships(@computer_submarine)
+
+    @computer_player.ships.each do |ship| # ships method lives in game file now
+      random_coordinates = @computer_board.cells.keys.sample(ship.length)
+
+      until @computer_board.valid_placement?(ship, random_coordinates)
+        random_coordinates = @computer_board.cells.keys.sample(ship.length)
+      end
+
+      @computer_board.place(ship, random_coordinates)
+    end
+  end
+
+  def computer_turn
+    puts "Computer is firing missile..."
+    comp_coordinate = @user_board.cells.keys.sample(1).join
+
+    until @user_board.valid_coordinate?(comp_coordinate) && !@user_board.cells[comp_coordinate].fired_upon?
+      comp_coordinate = @user_board.cells.keys.sample(1).join
+    end
+
+    comp_coordinate
+
+    @user_board.cells[comp_coordinate].fire_upon
+
+    if @user_board.cells[comp_coordinate].render == "M"
+      result = "miss."
+    elsif @user_board.cells[comp_coordinate].render == "H"
+      result = "hit!"
+    elsif @user_board.cells[comp_coordinate].render == "X"
+      result = "hit and sunk your #{@user_board.cells[comp_coordinate].ship.name}!"
+    end
+
+    puts "\nComputer shot on #{comp_coordinate} was a #{result}\n\n"
+
+    if @user_player.ships.all? { |ship| ship.sunk? }
+      puts "Computer wins!\n\n"
+      puts "Would you like to play again?\n\n"
+      main_menu
+    else
+      @game.user_turn # This may be an issue!!! check accuracy of return
+    end
+  end
 end
